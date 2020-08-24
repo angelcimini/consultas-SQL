afbd93fb9d0d150878c3f931684082435fe7e77b
angelcimini <66746746+angelcimini@users.noreply.github.com>
2020-08-24 22:18:58 +0200

    Delete conexionvideo.php

--- conexionvideo.php
+++
@@ -1,8 +0,0 @@
-<?php
-	//conexión a la base de datos
-	//host, usuario, password, base de datos
-	$conexionVideo = mysqli_connect('localhost', 'root', '', 'video') or die("hubo un error al conectar con la base de datos");
-
-	//juego de caracteres a utilizar en la conexión
-	mysqli_set_charset($conexionVideo, "utf8");
-?>
